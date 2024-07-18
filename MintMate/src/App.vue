b236eb29330d4d36aac3f3798993b0a0d118e55c
Aiala Del Olmo <aialadelolmo@gmail.com>
2024-07-18 13:06:10 +0200

    Splashscreen

--- MintMate/src/App.vue
+++ MintMate/src/App.vue
@@ -1,18 +1,42 @@
 <template>
-  <Home></Home>
+  <Suspense>
+    <template #default>
+      <Home></Home>
+    </template>
+    <template #fallback>
+      <splashscreen></splashscreen>
+    </template>
+  </Suspense>
 </template>
 
 <script >
-  import Home from './components/Home.vue';
-
+  import splashscreen from './components/SplashScreen.vue';
+  import { defineAsyncComponent } from 'vue';
   export default {
     components: {
-      Home,
-    }
+      splashscreen,
+      Home: defineAsyncComponent(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(import("./components/Home.vue"));
+        }, 2500);
+      });
+    })
+        }
   }
 </script>
 
 
 <style>
-
+  html,
+  body,
+  .app {
+    min-height: 100vh;
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  * {
+    --brand-green: #04b500;
+    --brand-blue: #0689b0;
+  }
 </style>
